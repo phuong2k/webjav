9a0ecde0cf4e718a283c091a9f6b4887307dec42
phuong <phuong.kizaru@gmail.com>
2023-06-06 18:56:28 +0700

    tuan dau buoi vlxx

--- src/components/IsamiGiken/index.js
+++ src/components/IsamiGiken/index.js
@@ -22,30 +22,61 @@ export default class IsamiGiken extends Component {
       slidesToScroll: 1
     };
     return (
-      <div className='container'>
+      <div className='content'>
+        <div className="content__header">
+          <div className="content__header--style">
+            <h1>IsamiGiken</h1><span></span><h2>功美技建</h2>
+          </div>
+          <div className="content__title">
+            <p>屋根・外壁の補修・雨漏り修理リフォームのご相談ならお任せ！</p>
+            <p>建築のことなら何でもご相談ください！</p>
+          </div>
+        </div>
         <Slider ref={c => (this.slider = c)} {...settings}>
           <div key={1} className='img__container'>
-            <img src='https://isamigiken.gattscom.com/image/top/ss_slider_img1.png' alt='img'></img>
+            <img className="img__main" src='https://isamigiken.gattscom.com/image/top/ss_slider_img1.png' alt='img'>
+            </img>
+            <div className="img__icon">
+              <img src='https://isamigiken.gattscom.com/image/top/ss_slider_icon1.png' alt='img'>
+              </img>
+              <span>古い外壁を新しくしたい</span>
+            </div>
           </div>
           <div key={2} className='img__container'>
-            <img src='https://isamigiken.gattscom.com/image/top/ss_slider_img2.png' alt='img'></img>
+            <img className="img__main" src='https://isamigiken.gattscom.com/image/top/ss_slider_img2.png' alt='img'></img>
+            <div className="img__icon">
+              <img  src='https://isamigiken.gattscom.com/image/top/ss_slider_icon2.png' alt='img'>
+              </img>
+              <span>屋根の劣化が気になる</span>
+            </div>
           </div>
           <div key={3} className='img__container'>
-            <img src='https://isamigiken.gattscom.com/image/top/ss_slider_img3.jpg' alt='img'></img>
+            <img className="img__main" src='https://isamigiken.gattscom.com/image/top/ss_slider_img3.jpg' alt='img'></img>
+            <div className="img__icon">
+              <img src='https://isamigiken.gattscom.com/image/top/ss_slider_icon3.png' alt='img'>
+              </img>
+              <span>納屋の壁を綺麗にしたい</span>
+            </div>
+
           </div>
           <div key={4} className='img__container'>
-            <img src='https://isamigiken.gattscom.com/image/top/ss_slider_img4.jpg' alt='img'></img>
+            <img className="img__main" src='https://isamigiken.gattscom.com/image/top/ss_slider_img4.jpg' alt='img'></img>
+            <div className="img__icon">
+              <img src='https://isamigiken.gattscom.com/image/top/ss_slider_icon4.png' alt='img'>
+              </img>
+              <span>ベランダで水漏れがある</span>
+            </div>
           </div>
-        </Slider>
-        <div style={{ textAlign: "center" }}>
-          <button className="button" onClick={this.previous}>
-            Previous
+        </Slider >
+        <div className="button__container" style={{ textAlign: "center" }}>
+          <button className="button pre" onClick={this.previous}>
+            {'<'}
           </button>
-          <button className="button" onClick={this.next}>
-            Next
+          <button className="button next" onClick={this.next}>
+            {'>'}
           </button>
         </div>
-      </div>
+      </div >
     );
   }
 }
